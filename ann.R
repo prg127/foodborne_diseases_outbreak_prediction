2525ce62dfaccba7717076eda473c5d36d09bc21
Pranav Goyal <42277863+prg127@users.noreply.github.com>
2019-04-08 01:02:47 +0530

    Add files via upload

---
+++ ann.R
@@ -0,0 +1,42 @@
+# Artificial Neural Network
+
+# Importing the dataset
+dataset = read.csv('cleaneddata.csv')
+dataset$X <-NULL
+dataset = dataset[c(2,3,4,5,6,7,8,9,12)]
+
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Status, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-9] = scale(training_set[-9])
+test_set[-9] = scale(test_set[-9])
+
+# Fitting ANN to the Training set
+# install.packages('h2o')
+library(h2o)
+h2o.init(nthreads = -1)
+model = h2o.deeplearning(y = 'Status',
+                         training_frame = as.h2o(training_set),
+                         activation = 'Rectifier',
+                         hidden = c(5,5),
+                         epochs = 100,
+                         train_samples_per_iteration = -2)
+
+# Predicting the Test set results
+y_pred = h2o.predict(model, newdata = as.h2o(test_set[-9]))
+
+y_pred_final = (y_pred > 0.5)
+
+y_pred_final = as.vector(y_pred_final)
+# Making the Confusion Matrix
+cm = table(test_set[, 9], y_pred_final)
+
+cm
+# h2o.shutdown()
